9976ade8d9b4d93e9d7142b95228962c6ab710f9
Rohan Thapa <rohanthapa632@gmail.com>
2022-07-22 14:37:39 +0545

    Delete bookself directory

--- bookself/database.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbuser = "root";
-$dbpass = "";
-$dbhost = "localhost";
-$dbname = "bookselfdb";
-
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname); // connecting with mysql with db
-?>
